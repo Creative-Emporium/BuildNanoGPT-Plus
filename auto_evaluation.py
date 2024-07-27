d88105b55d69601409ff0509be7db9d988a8dcab
Dr. Nicefellow <your_email@example.com>
2024-07-26 21:49:22 -0700

    update

--- auto_evaluation.py
+++ auto_evaluation.py
@@ -36,7 +36,7 @@ model_imp = 'custom'
 host = "http://127.0.0.1:5000/v1/chat/completions"
 LLMChatter = llm_chatter(host)
 
-first_prompt ='Please generate 50 pieces of texts for an LLM to continue to complete to test the completion abilities of an LLM. Please directly give the samples without numbers, each a line without any other comments.'
+first_prompt ='Please generate 50 pieces of texts that may appear in natural text for an LLM to continue to complete to test the completion abilities of an LLM. Please directly give the samples without numbers, each a line without any other comments.'
 
 
 answer = LLMChatter.communicate(first_prompt,reset=True)
@@ -44,6 +44,32 @@ answer = LLMChatter.communicate(first_prompt,reset=True)
 print("Answer: ", answer)
 samples = answer.split('\n')
 samples = [sam for sam in samples if len(sam) > 10]
+
+#win1 = 0
+#win2 = 0
+#i = 0
+#for sam in samples:
+#    comp1 = completion(model, enc, sam, device, device_type, model_imp, generate_max_length, num_return_sequences,
+#                       greedy=True)
+#    comp2 = completion(model2, enc, sam, device, device_type, model_imp, generate_max_length, num_return_sequences,
+#                       greedy=True)
+#    comp1 = comp1[0]
+#    comp2 = comp2[0]
+#
+#    prompt = f'Below are two completions from two LLMs from the prompt "{sam}". \nThe first completion is "{comp1}". \nThe second completion is "{comp2}".\n Please verify which one is better. Please start your answer with 1 or 2 meaning the first or the second completion is better (because I will extract your choice from the first 5 characters), followed by reason. Even if neither makes much sense, please still say which one is slightly better.'
+#    # print(f'-----------\nprompt:\n{prompt}----------------\n')
+#
+#    answer = LLMChatter.communicate(prompt, greedy=True, reset=True)
+#    print("=====\nAnswer: ", answer)
+#    if '1' in answer[:5]:
+#        win1 += 1
+#    elif '2' in answer[:5]:
+#        win2 += 1
+#    else:
+#        print("answer wrong")
+#
+#print(win1, win2)
+
 
 win1 = 0
 win2 = 0
